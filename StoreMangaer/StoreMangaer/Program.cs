a124dfce7856b04b4a460677fe1643521edafa1f
Glenn Cuevas <glenn.cuevas+github@gmail.com>
2020-08-04 11:41:52 -0700

    Store Manager with Core Project References

---
+++ StoreMangaer/StoreMangaer/Program.cs
@@ -0,0 +1,18 @@
+﻿using Core.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreMangaer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Store store = Core.DAL.StoreRepo.Stores.GetStore(1);
+			Console.WriteLine($"Loaded a store.\n ID: {store.ID }\n Name: {store.Name}");
+		}
+	}
+}
